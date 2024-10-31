3ea9f3568209c8909605f14885f71bc13b2f5923
royalfluff <wreckedlines@gmail.com>
2024-10-31 16:31:04 +0530

    updated, without error checks

--- Virtual Gallery project/main_code.py
+++ Virtual Gallery project/main_code.py
@@ -6,7 +6,6 @@ from urllib.request import urlopen
 
 API_KEY = "11505413-2a12-48e2-8123-3743409c7c4d"
 API_BASE_URL = " https://api.harvardartmuseums.org/object?"
-MIRADOR_URL = "https://iiif.harvardartmuseums.org/viewers/mirador/v2"
 
 
 def search_objects(search_term):
@@ -14,7 +13,6 @@ def search_objects(search_term):
         "keyword":search_term,
         "apikey": API_KEY,  
         "hasimage":1,
-        "century": "18th century"
     }
     
     response = requests.get(API_BASE_URL, params=params)
@@ -28,45 +26,13 @@ def display_results(search_results):
             iiif_url = f"{object['images'][0]['iiifbaseuri']}/full/full/0/default.jpg"
             print("Image URL:", iiif_url)
             
-            # Fetch image data from URL with User-Agent header
-            headers = {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36"
-            }
+            # Open the image with Pillow
+            im = Image.open(urlopen(iiif_url))
 
-            # Retry fetching the image if it is still processing
-            max_retries = 3
-            for attempt in range(max_retries):
-                #time.sleep(5)  # Allow time for processing
+            break  # Exit loop on success
 
-                # Fetch image data from URL
-                image_response = requests.get(iiif_url, headers=headers)
-                #print("Image Status Code:", image_response.status_code)
-
-                if image_response.status_code:
-                    try:
-                        
-                        # Open the image with Pillow
-                        # im = Image.open(urlopen(iiif_url))
-                        im = Image.open(requests.get(iiif_url, stream=True).raw)
-
-                        #im = Image.open(BytesIO(image_response.content))
-                        print(f"Image Format: {im.format}, Size: {im.size}, Mode: {im.mode}")
-                        break  # Exit loop on success
-                    
-                    except Exception as e:
-                        print(f"Failed to open image with Pillow: {e}")
-                        break  # Exit on error
-                
-                elif image_response.status_code == 202:
-                    print("Image is still processing, retrying...")
-                    #time.sleep(5)
-                
-                else:
-                    print(f"Failed to retrieve image: {iiif_url}, Status Code: {image_response.status_code}")
-                    break  # Exit loop on other failures
-        
-        else:
-            print("No images found for this object.")
+    else:
+        print("No images found for this object.")
 
 
 while True:
